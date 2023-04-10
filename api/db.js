723d573a6192f22d775c3da171429ec052aba40d
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-04-10 20:33:49 -0300

    nova funcionadade

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "crud2"
+    host: "192.185.176.163",
+    user: "engete23_root_condominio",
+    password: "Rk]8$3mz#voW]nRT",
+    database: "engete23_totem_engetera_db"
 })
