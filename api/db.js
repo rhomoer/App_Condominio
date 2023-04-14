466b131023d09daf704201ce67696d397d6e5fbd
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-04-14 00:16:33 -0300

    db issue

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host: "192.185.176.163",
-    user: "engete23_root_condominio",
-    password: "Rk]8$3mz#voW]nRT",
+    user: "engete23_root_eg",
+    password: "jppHSKPnGyH57CN",
     database: "engete23_totem_engetera_db"
 })
