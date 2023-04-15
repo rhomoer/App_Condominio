2b92ba23ccba9b814159e0eedb3eadfe784890ff
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-04-15 02:05:48 -0300

    ajuste de cors

--- api/index.js
+++ api/index.js
@@ -5,7 +5,18 @@ import cors from "cors"
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
+//const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
+
+//app.use(cors())
 
 app.use("/", userRoutes)
 
