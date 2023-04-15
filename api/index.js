fb8af2e505b006930b5bd0b1ec03ecab3f6c5a41
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-04-15 02:10:15 -0300

    .

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 //const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
