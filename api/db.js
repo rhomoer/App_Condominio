eac6842a1a0f8042c75447d55682cb1a271b2733
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-07-03 11:59:33 -0300

    produção

--- api/db.js
+++ api/db.js
@@ -1,14 +1,16 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  /*
+    database: "engete23_totem_engetera_db",
+    
+    
     host: "192.185.176.163",
     user: "engete23_root_eg",
-    password: "jppHSKPnGyH57CN",
-    */
-    database: "engete23_totem_engetera_db",
+    password: "jppHSKPnGyH57CN"
+ /*
+    
     host: "localhost",
     user: "root",
     password: "password"
-
+*/
 })
