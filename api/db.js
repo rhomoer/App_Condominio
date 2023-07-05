cdc2690c2ccc33593ab4645e4b23689bfec98362
rodrigodsouza@hotmail.com <rodrigodsouza@hotmail.com>
2023-07-05 01:08:46 -0300

    .env updating

--- api/db.js
+++ api/db.js
@@ -3,17 +3,17 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     database: "engete23_totem_engetera_db",
     
-    
+ /*   
     host: process.env.REACT_APP_HOST,
     user: process.env.REACT_APP_USER,
     password:process.env.REACT_APP_PASSWORD
- /*
  
+ */
   host: "192.185.176.163",
     user: "engete23_root_eg",
     password: "jppHSKPnGyH57CN"
  
-
+/*
     host: "localhost",
     user: "root",
     password: "password"
