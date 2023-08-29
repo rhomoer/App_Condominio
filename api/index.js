f9789fb67cc90ff4911e7f57a40e60127ca25b3c
rhomoer <rodrigodsouza@hotmail.com>
2023-08-29 15:55:03 -0300

    ......

--- api/index.js
+++ api/index.js
@@ -6,15 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(cors({ origin: [
-    "http://localhost:3000",
-    "https://localhost:8800",
-    "http://localhost:3000", 
-    "https://localhost:8800",
-"splendor.engetera.com.br:8800",
- "https://splendor.engetera.com.br:8800",
-"http://localhost:3000", 
-"http://splendor.engetera.com.br:8800"] }));
+
 
 
 app.use("/", userRoutes)
